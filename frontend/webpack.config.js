2f73cf7937a7e3678218f84820204874316d3232
Anil <anil@protonmail.ch>
2017-06-15 18:34:26 +0200

    Updated webpack to v2
    Now using babel-preset-env
    Added loaders for images and fonts
    We can now use the latest docs for webpack ~ 2.6.0

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,44 +10,68 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
 
     entry: {
-        'vendor': ['angular','angular-animate','angular-aria','angular-messages','angular-material','angular-material-icons','@uirouter/angularjs'],
-        'app': path.resolve(__dirname,'src/app.js')
+        'vendor': ['angular', 'angular-animate', 'angular-aria', 'angular-messages', 'angular-material', 'angular-material-icons', '@uirouter/angularjs'],
+        'app': path.resolve(__dirname, 'src/app.js')
     },
     output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'scripts/[name].js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'scripts/[name].bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
+            // Load JS Files
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
-                loader: 'babel', // 'babel-loader' is also a legal name to reference
-                query: {
-                    presets: ['es2015']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    }
                 }
             },
+            // Load HTML
             {
                 test: /\.html$/,
-                loader: 'html'
+                loader: 'html-loader'
             },
-            // Extract css files
+            // Load CSS
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            }
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            // Load Images
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            // Load Fonts
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
         ]
     },
 
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
-            name: ['app', 'vendor']
+            name: ['app', 'vendor'],
+            async: true
         }),
 
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname,'src/index.html')
+            title: 'Testing',
+            template: path.resolve(__dirname, 'src/index.html')
         }),
 
         new ExtractTextPlugin("styles/[name].css"),
+        new webpack.BannerPlugin("SEBA-TEAM24"),
     ]
 };
