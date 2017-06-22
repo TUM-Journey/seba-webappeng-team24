fd3931cb86680ffcc8ffb43280a28161fefbf937
Anil <anil@protonmail.ch>
2017-06-22 16:31:17 +0200

    Feature/backend-JWT-based-login-registration (#48)
    * Implemented JWT based Authentication
    
    Some methods in the api have been moved around, api still dirty but works. Added a db dump for local development (without docker)
    
    Made login function async
    
    * Added basic company related functions, models
    
    * Minor readme change about db
    
    * Removed bcrypt, added bcryptjs
    
    bcrypt is causing problems while installing on a fresh system. added the alternative lib with the identical api
    
    * Removed database dump
    
    * Removed package-lock.json

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,21 +1,20 @@
-import {version} from '../../package.json';
-import {Router} from 'express';
+import { version } from '../../package.json';
+import { Router } from 'express';
 
 import users from './users';
 import forms from './forms';
 import feedbacks from './feedbacks';
 
-export default ({config, db}) => {
+export default ({ config, db }) => {
     let api = Router();
 
     // Mount API Resources
-    api.use('/users', users({config, db}));
-    api.use('/forms', forms({config, db}));
-    api.use('/feedbacks', feedbacks({config, db}));
-
+    api.use('/users', users({ config, db }));
+    api.use('/forms', forms({ config, db }));
+    api.use('/feedbacks', feedbacks({ config, db }));
     // API Metadata
-    api.get('/', (req, res) => {
-        res.json({version});
+    api.get('/version', (req, res) => {
+        res.json({ version });
     });
 
     return api;
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,18 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import initializeDb from './db';
 import api from './api';
-import config from './config.json';
+
+import config from './config.json'
+import * as auth from './api/auth'
+import passport from 'passport'
+
 import { isProduction } from './config'
+
 
 let app = express();
 app.server = http.createServer(app);
+passport.use(auth.JWTStrategy)
+app.use(passport.initialize())
 
 // logger
 app.use(morgan('dev'));
@@ -23,15 +30,25 @@ app.use(bodyParser.json({
 	limit: config.bodyLimit
 }));
 
+
+app.use(bodyParser.urlencoded({
+	extended: true,
+}))
+
+app.post("/login", auth.login)
+app.post("/register", auth.register)
+app.post("/company", auth.companyRegister)
+
 if (isProduction) {
 	app.use(express.static('/dist'))
 }
 
+
 // connect to db
 initializeDb(db => {
 	// api router
-	app.use('/api', api({ config, db }));
-
+	app.use('/api', passport.authenticate('jwt', { session: false }),
+		api({ config, db }));
 	app.server.listen(process.env.PORT || config.port, () => {
 		console.log(`Started on port ${app.server.address().port}`);
 	});
