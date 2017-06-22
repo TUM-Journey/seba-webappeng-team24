258cb86541fbb01c5514aef4126eb4d4e5ea8052
Anil <anil@protonmail.ch>
2017-06-22 11:43:19 +0200

    Fixed the minor rebase mistake, and added a line to serve frontend dist folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,36 @@
-
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import initializeDb from './db';
+import api from './api';
 import config from './config.json';
+import { isProduction } from './config'
+
+let app = express();
+app.server = http.createServer(app);
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(cors({
+	exposedHeaders: config.corsHeaders
+}));
+
+app.use(bodyParser.json({
+	limit: config.bodyLimit
+}));
+
+if (isProduction) {
+	app.use(express.static('/dist'))
+}
+
+// connect to db
+initializeDb(db => {
+	// api router
+	app.use('/api', api({ config, db }));
 
 	app.server.listen(process.env.PORT || config.port, () => {
 		console.log(`Started on port ${app.server.address().port}`);
