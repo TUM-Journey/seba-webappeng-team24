832e027c934f35540a7fd3a706d43a3fe60c3600
Burak Cemil <tangiersdrums@gmail.com>
2017-07-16 12:49:54 +0200

    Small message fix.

--- frontend/src/components/view-login/view-login.component.js
+++ frontend/src/components/view-login/view-login.component.js
@@ -36,7 +36,7 @@ class ViewLoginComponentController {
       this.$state.go('dashboard.feedback-mine-inbound', {});
       this.$mdToast.show(
         this.$mdToast.simple()
-          .textContent('Welcome back, ' + username)
+          .textContent('Welcome back, ' + username + '.')
           .position('top center')
           .capsule(true)
           .hideDelay(4000));
