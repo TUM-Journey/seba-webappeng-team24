a9d1fd14c7cd6505a5263dee22a75f602ff90a0f
Anil <anil@protonmail.ch>
2017-07-17 04:29:17 +0200

    This is necessary for the prod env to work,
    I have no idea WHY

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -86,7 +86,7 @@ module.exports = {
 
     plugins: [
         new webpack.EnvironmentPlugin({
-            'url': '',
+            'url': 'api',
         }),
         // new UglifyJSPlugin(),
         new webpack.optimize.CommonsChunkPlugin({
